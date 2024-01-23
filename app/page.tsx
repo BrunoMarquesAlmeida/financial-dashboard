ce5a0b88cccf26d4956189ee6d3d907da939b928
BrunoMarquesAlmeida <brunom.almeida@hotmail.com>
2024-01-23 16:54:09 +0000

    Revert "create and import data"
    This reverts commit 77b8a82c51f7767ea0a2ebb93d3853518ceb62d5.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import data from "../data.json";
-
-import * as d3 from "d3";
-
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Home
+    </main>
+  );
 }
